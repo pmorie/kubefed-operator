fb0f89d2ddab430c1f3fa29cd9f28c4f9a2cff67
Aniket Bhat <anbhat@redhat.com>
2019-05-23 15:54:07 -0400

    copy deploy directory fix the getopts parsing for install.sh update clusterrolebindings.yaml

--- install.sh
+++ install.sh
@@ -4,18 +4,21 @@
 # usage ./install.sh <location> <namespace>
 
 set -e
+ 
 
 NAMESPACE=""
 LOCATION="local"
 NAMESPACE_STR=""
 
-while getopts “:n:d” opt; do
+while getopts “n:d:” opt; do
     case $opt in
 	n) NAMESPACE=$OPTARG ;;
 	d) LOCATION=$OPTARG ;;
     esac
 done
 
+echo "NS=$NAMESPACE"
+echo "LOC=$LOCATION"
 
 if test X"$NAMESPACE" != X; then
     # create a namespace 
@@ -28,11 +31,16 @@ for f in ./deploy/crds/*_crd.yaml ; do
   kubectl apply -f "${f}" ; 
 done
 
+# Install CR
+for f in ./deploy/crds/*_cr.yaml; do
+    kubectl apply -f "${f}" $NAMESPACE_STR
+done
+
 # Check if operator-sdk is installed or not and accordinlgy execute the command.
-if test X"$LOCATION" = X"local"; then
+if test X"$LOCATION" = Xlocal; then
     operator-sdk &> /dev/null
     if [ $? == 0 ]; then
-	operator-sdk up local $NAMESPACE_STR
+	operator-sdk up local $NAMESPACE_STR &
     else
 	echo "Operator SDK is not installed."
 	exit 1
@@ -48,4 +56,6 @@ else
     done
     echo "Deployed all the operator yamls for kubefed-operator in the cluster"
 fi
+
+
 
